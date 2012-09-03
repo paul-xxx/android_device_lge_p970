df97a60c16502df5d4bc9df6e91286a086d5e558
paul-xxx <thepaulxxx@gmail.com>
2012-09-03 12:41:04 +0600

    device: add new packages, update some others

--- p970.mk
+++ p970.mk
@@ -25,7 +25,9 @@ PRODUCT_PACKAGES += \
 
 # Device specific
 PRODUCT_PACKAGES += \
-    hwcomposer.omap3
+    hwcomposer.p970 \
+    camera.p970 \
+    lights.p970
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -37,6 +39,12 @@ PRODUCT_PACKAGES += \
     libomap_mm_library_jni \
     libRS \
     librs_jni
+
+# Wifi
+PRODUCT_PACKAGES += wifimac
+
+# Recovery
+PRODUCT_PACKAGES += prb
 
 # OpenMAX IL configuration
 PRODUCT_COPY_FILES += \
@@ -54,15 +62,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # These are the hardware-specific settings that are stored in system properties.
 # Note that the only such settings should be the ones that are too low-level to
@@ -75,5 +81,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # half of the device-specific product definition file takes care
 # of the aspects that require proprietary drivers that aren't
 # commonly available
-#$(call inherit-product-if-exists, vendor/lge/p970/device-vendor.mk)
-#$(call inherit-product-if-exists, vendor/lge/p970/p970-device-vendor.mk)
+#$(call inherit-product-if-exists, vendor/lge/p970/p970-vendor.mk)
+
+# Include vendor non-open source blobs
+#$(call inherit-product-if-exists, vendor/lge/p970/p970-vendor-blobs.mk)
