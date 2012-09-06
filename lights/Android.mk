2d07fc399e1ba2e89f6631b9aff9fdad12b7b335
paul-xxx <thepaulxxx@gmail.com>
2012-09-07 00:57:43 +0600

    lights: fix module tags

--- lights/Android.mk
+++ lights/Android.mk
@@ -17,7 +17,6 @@ LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := eng
 
 LOCAL_SRC_FILES := lights.c
 
@@ -27,5 +26,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := lights.p970
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
