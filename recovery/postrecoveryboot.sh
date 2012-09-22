9a75800ec84112b1df16a1f7b525b7a4b7b9a995
paul-xxx <thepaulxxx@gmail.com>
2012-09-23 00:28:29 +0600

    ramdisk and recovery update

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -9,3 +9,4 @@ dd if=/dev/zero of=/dev/block/mmcblk0p12 seek=512 count=13 bs=1
 dd if=/dev/zero of=/dev/block/mmcblk0p12 seek=576 count=20 bs=1
 
 echo 0 > /sys/devices/platform/i2c_omap.2/i2c-2/2-001a/blink_enable
+
