80b60ed61f4ee0214048ef3466358cf9ee27a8f4
paul-xxx <thepaulxxx@gmail.com>
2013-06-23 23:35:25 +0600

    updated: - kernel - add power hal - add light hal - wlan update - lun file add

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,24 @@
-#USE_CAMERA_STUB := true
+#
+# Copyright (C) 2013 BPaul
+#
 
 # inherit from the proprietary version
 -include vendor/lge/p970/BoardConfigVendor.mk
 
-TARGET_BOARD_PLATFORM := omap3
+TARGET_SPECIFIC_HEADER_PATH := device/lge/p970/include
+
+# Kernel properties
+TARGET_KERNEL_SOURCE := kernel/lge/p970
+TARGET_KERNEL_CONFIG := on_ubuntu_p970_defconfig
+
+# Kernel information
+BOARD_KERNEL_CMDLINE := # This is ignored by lge's bootloader
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_RECOVERY_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
 
 # Board configuration
-#
+TARGET_BOARD_PLATFORM := omap3
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
 TARGET_CPU_ABI := armeabi-v7a
@@ -14,33 +26,24 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 OMAP_ENHANCEMENT := true
-
 TARGET_BOOTLOADER_BOARD_NAME := p970
-
-BOARD_KERNEL_CMDLINE :=
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_PAGE_SIZE := 0x00000800
-
-ifdef OMAP_ENHANCEMENT
 COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP3 -DOMAP_ENHANCEMENT_CPCAM -DOMAP_ENHANCEMENT_VTC
-endif
 
-# Try to build the kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_p970_defconfig
-# Keep this as a fallback
-TARGET_PREBUILT_KERNEL := device/lge/p970/kernel
-
-BOARD_NEEDS_CUTILS_LOG := true
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-# Use this flag if the board has a ext4 partition larger than 2gb
-#BOARD_HAS_LARGE_FILESYSTEM := true
-
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Light HAL
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# Power HAL
+TARGET_PROVIDES_POWERHAL := true
+
+# FM
 BOARD_HAVE_FM_RADIO := true
 
-# BOARD_USES_TI_CAMERA_HAL := true
+# Camera
+# BOARD_USES_TI_CAMERA_HAL := true # Need improvement
 HARDWARE_OMX := true
 ifdef HARDWARE_OMX
 OMX_JPEG := true
@@ -51,44 +54,50 @@ OMX_VENDOR_INCLUDES := \
 OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
 BOARD_OPENCORE_LIBRARIES := libOMX_Core
 BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
-#BOARD_CAMERA_LIBRARIES := libcamera
 endif
 
-BOARD_MOBILEDATA_INTERFACE_NAME := "vsnet0"
-
+# WLAN
 BOARD_WLAN_DEVICE := bcm4329
-WIFI_DRIVER_FW_PATH_STA         := "/system/etc/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP          := "/system/etc/firmware/fw_bcm4329_ap.bin"
-WIFI_DRIVER_MODULE_NAME         := "wireless"
-WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/wireless.ko"
-WIFI_DRIVER_MODULE_ARG          := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/nvram.txt config_path=/data/misc/wifi/config"
-WPA_SUPPLICANT_VERSION          := VER_0_8_X
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_NAME := "wireless"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wireless.ko"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/nvram.txt config_path=/data/misc/wifi/config"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wext
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-WIFI_DRIVER_HAS_LGE_SOFTAP      := true
+WIFI_DRIVER_HAS_LGE_SOFTAP := true
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 
-
-BOARD_EGL_CFG := device/lge/p970/configs/egl.cfg
-
+# Misc
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 665681920
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1170259968
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_NEEDS_CUTILS_LOG := true
 
-TARGET_SPECIFIC_HEADER_PATH := device/lge/p970/include
-
+# Display
 USE_OPENGL_RENDERER := true
-#BOARD_TOUCH_RECOVERY := true
-BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p970/recovery/graphics.c
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_EGL_CFG := device/lge/p970/configs/egl.cfg
 
+# Recovery
+BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p970/recovery/graphics.c
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file"
+
+# Vibrator fix
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/lge/p970/vibrator.c
+
+# Enable compat with old blobs
 COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB -DICS_CAMERA_BLOB -DOMAP_ICS_CAMERA -DUSE_FENCE_SYNC
 
+# ALS
 BOARD_SYSFS_LIGHT_SENSOR := "/sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0060/leds/lcd-backlight/als"
 
+# Charger
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"rs"' -DBOARD_CHARGING_CMDLINE_VALUE='"c"'
-## Radio fixes
+
+# RIL fixes
 BOARD_RIL_CLASS := ../../../device/lge/p970/ril/
+BOARD_MOBILEDATA_INTERFACE_NAME := "vsnet0"
 
